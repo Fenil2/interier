7f6d3ee9468915480b3893d0d1d13f6b2e08c579
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-05-22 13:34:45 +0530

    initialssssl

--- components/fourwalls/Footer.tsx
+++ components/fourwalls/Footer.tsx
@@ -78,7 +78,7 @@ export default function Footer() {
 
       </div>
 
-      <MotionReveal delayMs={140} className="mx-auto mt-6 flex max-w-[1280px] flex-col justify-between gap-2 border-t border-[#1e3824] px-5 pt-4 text-[0.82rem] text-[#4e6858] md:flex-row md:px-10">
+      <div className="mx-auto mt-6 flex max-w-7xl flex-col justify-between gap-2 border-t border-[#1e3824] px-5 pt-4 text-[0.82rem] text-[#4e6858] md:flex-row md:px-10">
         <p>
           Copyright 2025 <span className="text-[#c8a856]">Fourwalls Interiors &amp; Constructions</span>. All rights reserved.
         </p>
@@ -87,7 +87,7 @@ export default function Footer() {
           <span>·</span>
           <span>Chennai, South India</span>
         </p>
-      </MotionReveal>
+      </div>
     </footer>
   );
 }
